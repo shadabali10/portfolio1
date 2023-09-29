c00181a5425fc2054799ac41f3a28b3e1af50f68
shadab2110058 <shadab2110058@akgec.ac.in>
2023-09-29 21:45:40 +0530

    completed

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Study Abroad Website",
+        description:"It is build on top of the React JS, with styledComponents and GSAP for smooth scrolling animations.",
+        tags:["react","gsap","styledComponents","tailwind"],
+        
+        demo:"https://deploy-preview-2--delightful-tanuki-d934c6.netlify.app/",
+        github:"https://github.com/shadabali10/AdviceOverseas"
+    },
+    {
+        id:2,
+        name:"Resume Builder Website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","MERN"],
+        
+        demo:"https://github.com/shadabali10/Resume-Builder-1",
+        github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Weather App",
+        description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
+        tags:["react","api"],
+        
+        demo:"https://shadabali10.github.io/weather/",
+        github:"https://github.com/shadabali10/weather"
+    },
+    {
+        id:4,
+        name:"Quiz App",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://shadabali10.github.io/quiz-app/",
+        github:"https://github.com/shadabali10/quiz-app"
+       
+    },{
+        id:5,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/React-Redux-Todo-App"
+        
+    },
+    {
+        id:6,
+        name:"Ecommerce ",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"https://shadabali10.github.io/Ecommerce/",
+        github:"https://github.com/shadabali10/Ecommerce"
+        
+    },
+    
+  
+] 
